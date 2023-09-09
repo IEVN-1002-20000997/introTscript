c4287f53729d729412b2fa8c1b7d4b081b180963
Diego Frausto <frausto.hernandez.diego@gmail.com>
2023-09-09 10:22:57 -0600

    segundo commit, tipos e interfaz

---
+++ src/ejemplos-ts/01-tipos-basicos.ts
@@ -0,0 +1,17 @@
+
+
+//variables
+
+var num1=1;
+let num2=5;
+const num3=4;
+
+
+let nombre:string='Diego';
+let num:number=23;
+let activo:boolean=true;
+
+let matricula:number | string | boolean;
+matricula=20000997;
+matricula='ywt-2882'
+matricula=false;
---
+++ src/ejemplos-ts/02-obj-interfaz-arre.ts
@@ -0,0 +1,17 @@
+
+
+
+interface Alumno{
+    matricula?:number;
+    nombre:string;
+    email:string;
+}
+
+const alumno:Alumno={
+    nombre:'juan',
+    email:'juan@gmail.com'
+
+}
+
+
+console.log(alumno.nombre);
